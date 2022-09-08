1f97d541d9e956aef5c862513ae2b945c10a025e
FarazPir <faraz.piracha@student.allenisd.org>
2022-09-08 00:17:12 -0500

    restored correct code

--- main.swift
+++ main.swift
@@ -1,7 +1,16 @@
-let wordInput = readLine()
-var wordArray = [] as [String]
-while wordInput != ""{
-    wordArray.append(wordInput)
+var index = 0 
+var wordArray = [String]()
+var word : String
+
+repeat {
+    word = readLine()!
+    wordArray.append(word)
+    index += 1
+} while word != ""
+wordArray.removeLast()
+
+for word in wordArray {
+    print(word)
 }
 
    
