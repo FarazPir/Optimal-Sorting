50f520a3f2bd6eb9bb6b7d92881b547290b2cfbc
FarazPir <faraz.piracha@student.allenisd.org>
2022-09-17 16:49:46 -0500

    Changed method of reading input

--- main.swift
+++ main.swift
@@ -1,28 +1,16 @@
 import Foundation
 
 //Defines wordArray, sortedWordArray, word, and input
-var wordArray : ArraySlice<Substring> = []
-var sortedWordArray : ArraySlice<Substring> = []
-var word : String
-let textPath = "random.txt"
+var wordArray = [String]()
+var sortedWordArray :  [String]
 
 
-do {
-    let contents = try NSString(contentsOfFile: textPath, encoding: String.Encoding.ascii.rawValue)
-    
-    let allWords = contents as String
-    let allWordsArray = allWords.split(separator: "\n")
-    wordArray = allWordsArray[..<20]
-    //print(wordArray)
 
-} catch {
-    print("contents couldn't be loaded")
-}
 
 
 
 //compares the ascii values in two strings to figure out if the first string is greater than the second string
-func stringGreaterThan(string1: Substring, string2: Substring) -> Bool {
+func stringGreaterThan(string1: String, string2: String) -> Bool {
     
     //filters out anything that isn't a letter to sort only the letters
     let s1 = string1.lowercased().filter("abcdefghijklmnopqrstuvwxyz".contains)
@@ -31,14 +19,14 @@ func stringGreaterThan(string1: Substring, string2: Substring) -> Bool {
 }
 
 // swaps two elements in an array
-func swap(strings: inout ArraySlice<Substring>, firstIndex: Int, secondIndex: Int) {
+func swap(strings: inout [String], firstIndex: Int, secondIndex: Int) {
     let temp = strings[firstIndex]
     strings[firstIndex] = strings[secondIndex]
     strings[secondIndex] = temp
     
 }
 //Uses bubble sorting to sort array
-func insertionSort(strings: ArraySlice<Substring>) -> ArraySlice<Substring> {
+func insertionSort(strings: [String]) -> [String] {
 
     var stringArray = strings
     for index in 1..<stringArray.count {
@@ -53,41 +41,13 @@ func insertionSort(strings: ArraySlice<Substring>) -> ArraySlice<Substring> {
 }
 
 
-/*
- 
-    var marker = 1
-    var stringArray = strings
-    var unsortedStrings = strings[marker...]
-    var sortedStrings = strings[..<marker]
-    var swaps = 0
-    var firstIndex = 0
-    var secondIndex = 1    
 
-    //print("funcition hapenigngn")
-    while !unsortedStrings.isEmpty  {
-        firstIndex = 0
-        secondIndex = 1
-        
-        while firstIndex >= 0 {
-            print("loop 2 happpend")
-        if stringGreaterThan(string1: stringArray[firstIndex], string2: stringArray[secondIndex]) {
-            swap(strings:&stringArray, firstIndex:firstIndex, secondIndex:secondIndex)
-            print("it swapped")
-            secondIndex = firstIndex
-        }
-
-        firstIndex -= 1
-
-        }
-        
-        marker += 1
-        unsortedStrings = stringArray[marker...]
-        sortedStrings = stringArray[..<marker]
-    }
-    
-    return sortedStrings
-    }
- */
+//checks to see if input is nil or blank then appends input and checks next input
+var word = readLine()
+while word != nil && word != "" {
+    wordArray.append(word!)
+    word = readLine()
+}
 
 
 
